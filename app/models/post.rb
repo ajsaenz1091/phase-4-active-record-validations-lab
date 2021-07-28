b474bc8b0c1624da89e16c7a5487e6301f6c3cc6
Alberto <ajsaenz1091@gmail.com>
2021-07-28 11:21:35 -0400

    Completed Assignments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,18 @@ class Post < ApplicationRecord
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 
-    validate :must_include_list
+    validate :clickbait?
 
-    def clickbait?
-
-    end
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
